83cdeb3ccc834829f72e09ee2e2f5018aafec6db
Yevhenii Pozniak <jekamanu@gmail.com>
2025-01-13 16:53:23 +0200

    add header navigation menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import styles from './page.module.css';
+import Container from './shared/Container/Container';
+
+export interface HomeProps {}
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home page</h1>
-    </main>
+    <header>
+      <Container>
+        <nav className={styles['header-nav']}>
+          <li className={styles['header-list']}>
+            <p className={styles['header-link']}>Hello</p>
+          </li>
+          <li className={styles['header-list']}>
+            <p className={styles['header-link']}>Menu</p>
+          </li>
+          <li className={styles['header-list']}>
+            <p className={styles['header-link']}>Gallery</p>
+          </li>
+          <li className={styles['header-list']}>
+            <p className={styles['header-link']}>Reservations</p>
+          </li>
+          <li className={styles['header-list']}>
+            <p className={styles['header-link']}>About</p>
+          </li>
+        </nav>
+      </Container>
+    </header>
   );
 }
