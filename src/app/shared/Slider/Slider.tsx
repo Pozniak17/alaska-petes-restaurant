417b5c6a4b9cc21e92fdd5a5a7252b31b9e4771b
Yevhenii Pozniak <jekamanu@gmail.com>
2025-01-27 17:17:34 +0200

    finished Events section and Slider inside section

--- src/app/shared/Slider/Slider.tsx
+++ src/app/shared/Slider/Slider.tsx
@@ -11,10 +11,12 @@ import styles from './Slider.module.css';
 import img1 from '../../../../public/image/event1.jpg';
 import img2 from '../../../../public/image/event2.jpg';
 import img3 from '../../../../public/image/event3.jpg';
+import arrow from '../../../../public/icons/down-arrow.svg';
 
 export default function Slider() {
   return (
     <Swiper
+      className={styles.test}
       modules={[Pagination, Autoplay]}
       spaceBetween={20}
       pagination={{ clickable: true }}
@@ -35,19 +37,52 @@ export default function Slider() {
         },
       }}
     >
-      <SwiperSlide>
-        <Image src={img1} alt="Event 1" />
-        <h3>Music Events</h3>
+      <SwiperSlide className={styles.wrapper}>
+        <div>
+          <Image src={img1} alt="Event 1" className={styles.image} />
+          <h3 className={styles.title}>Music Events</h3>
+          <div className={styles.text_wrapper}>
+            <p className={styles.text}>Concept</p>
+            <Image src={arrow} alt="arrow down" />
+          </div>
+
+          <div className={styles.text_wrapper}>
+            <p className={styles.text}>Service</p>
+            <Image src={arrow} alt="arrow down" />
+          </div>
+        </div>
       </SwiperSlide>
 
-      <SwiperSlide>
-        <Image src={img2} alt="Event 2" />
-        <h3>Music Events</h3>
+      <SwiperSlide className={styles.wrapper}>
+        <div>
+          <Image src={img2} alt="Event 2" className={styles.image} />
+          <h3 className={styles.title}>Music Events</h3>
+          <div className={styles.text_wrapper}>
+            <p className={styles.text}>Concept</p>
+            <Image src={arrow} alt="arrow down" />
+          </div>
+
+          <div className={styles.text_wrapper}>
+            <p className={styles.text}>Service</p>
+            <Image src={arrow} alt="arrow down" />
+          </div>
+        </div>
       </SwiperSlide>
 
-      <SwiperSlide>
-        <Image src={img3} alt="Event 3" />
-        <h3>Music Events</h3>
+      <SwiperSlide className={styles.wrapper}>
+        <div>
+          <Image src={img3} alt="Event 3" className={styles.image} />
+          <h3 className={styles.title}>Music Events</h3>
+          <div className={styles.text_wrapper}>
+            <p className={styles.text}>Concept</p>
+            <Image src={arrow} alt="arrow down" />
+          </div>
+
+          <div className={styles.text_wrapper}>
+            <p className={styles.text}>Service</p>
+            <Image src={arrow} alt="arrow down" />
+          </div>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
