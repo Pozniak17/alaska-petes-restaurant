fa190dc588e7273c0a41a3800afd95a24df95b0a
Yevhenii Pozniak <jekamanu@gmail.com>
2025-01-28 15:54:50 +0200

    finished section Footer: mobile/table/desktop

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from './Footer.module.css';
 import map from '../../../../public/icons/footer/map.svg';
 import mail from '../../../../public/icons/footer/mail.svg';
-import ring from '../../../../public/icons/footer/ring.svg';
+import phone from '../../../../public/icons/footer/ring.svg';
 import clock from '../../../../public/icons/footer/clock.svg';
 import telegram from '../../../../public/icons/footer/telegram.svg';
 import facebook from '../../../../public/icons/footer/facebook.svg';
@@ -15,63 +15,75 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Container>
-        <div className={styles.footer_wrapper}>
-          <Image src={map} alt="map icon" />
-          <div className={styles.footer_address}>
-            <p className={styles.footer_text}>
-              Alaska Pete&apos;s, USA 151 Seven Bridge Road
-            </p>
-            <p className={styles.footer_text}>Marshalls Creek, PA 18335</p>
+        <div className={styles.main_wrapper}>
+          <div className={styles.first_wrapper}>
+            <div className={styles.address_wrapper}>
+              <Image src={map} alt="map icon" />
+              <ul className={styles.address_list}>
+                <li>
+                  <p>Alaska Pete's, USA 151 Seven Bridge Road</p>
+                </li>
+                <li>
+                  <p>Marshalls Creek, PA 18335</p>
+                </li>
+              </ul>
+            </div>
+
+            <div className={styles.contact_wrapper}>
+              <ul className={styles.contact_list}>
+                <li className={styles.contact_item}>
+                  <Image
+                    src={mail}
+                    alt="@mail icon"
+                    className={styles.contact_icon}
+                  />
+                  <p className={styles.contact_text}>destination@gmail.com</p>
+                </li>
+
+                <li className={styles.contact_item}>
+                  <Image src={phone} alt="phone number" />
+                  <p className={styles.contact_text}>(570) 223-8575</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className={styles.second_wrapper}>
+            <div className={styles.hours_container}>
+              <Image src={clock} alt="clock icon" />
+              <ul className={styles.hours_list}>
+                <li>
+                  <p>Open Hours</p>
+                </li>
+                <li>
+                  <p>Mon - Sun: 12am - 12am</p>
+                </li>
+              </ul>
+            </div>
+
+            <ul className={styles.icon_list}>
+              <li>
+                <Image src={telegram} alt="telegram icon" />
+              </li>
+              <li>
+                <Image src={facebook} alt="facebook icon" />
+              </li>
+              <li>
+                <Image src={youtube} alt="youtube icon" />
+              </li>
+              <li>
+                <Image src={instagram} alt="instagram icon" />
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.logo}>
+            <Image src={logo} alt="logo" /> Alaska Pete’s
           </div>
         </div>
-
-        <div className={styles.contact_wrapper}>
-          <div className={styles.footer_wrapper}>
-            <a href="mailto:destination@gmail.com" className={styles.link}>
-              <Image src={mail} alt="map icon" className={styles.icon} />
-              <span className={styles.link_text}>destination@gmail.com</span>
-            </a>
-          </div>
-
-          <div className={styles.footer_wrapper}>
-            <a href="telto:(570) 223-8575" className={styles.link}>
-              <Image src={ring} alt="phone icon" />
-              <span className={styles.link_text}>(570) 223-8575</span>
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.hours_wrapper}>
-          <Image src={clock} alt="clock icon" className={styles.icon} />
-          <div>
-            <p className={styles.footer_text}>Open Hours</p>
-            <p className={styles.footer_text}>Mon - Sun: 12am - 12am</p>
-          </div>
-
-          <ul className={styles.icon_list}>
-            <li>
-              <Image src={telegram} alt="telegram icon" />
-            </li>
-            <li>
-              <Image src={facebook} alt="facebook icon" />
-            </li>
-            <li>
-              <Image src={youtube} alt="youtube icon" />
-            </li>
-            <li>
-              <Image src={instagram} alt="instagram icon" />
-            </li>
-          </ul>
-        </div>
-
-        <div className={styles.footer_logo}>
-          <Image src={logo} alt="logo icon" />
-          <h3>Alaska Pete’s</h3>
-        </div>
-
-        <p className={`${styles.footer_text} ${styles.coopyrigth}`}>
+        <div className={styles.copyright}>
           CatCodeStudio @ 2025. All rights reserved.
-        </p>
+        </div>
       </Container>
     </footer>
   );
