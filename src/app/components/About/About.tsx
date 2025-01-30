a1406ae90246bd1b3a73b59984508c7a49982a77
Yevhenii Pozniak <jekamanu@gmail.com>
2025-01-30 17:15:19 +0200

    update section About: Desktop

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -8,41 +8,37 @@ export default function About() {
       <h2 className={styles.title}>Meet the Chief</h2>
       <div className={styles.wrapper}>
         <div className={styles.first_wrapper}>
-          <div className={styles.image_wrapper}>
+          <div>
             <Image src={chef} alt="chef's photo" className={styles.image} />
           </div>
 
-          <div className={styles.second_wrapper}>
-            <div className={styles.about_wrapper}>
-              <h3 className={styles.about_name}>Alice Waters</h3>
-              <p className={styles.about_text}>
-                Alice Waters is a celebrated American chef, restaurateur,
-                author, and activist known for her dedication to sustainable,
-                locally sourced food. She founded Chez Panisse, a famed Berkeley
-                restaurant that embodies the farm-to-table philosophy. Waters
-                emphasizes fresh, organic ingredients from local producers who
-                prioritize environmental care, transforming how people think
-                about food and its connection to the environment.
-              </p>
+          <div className={styles.about_wrapper}>
+            <h3 className={styles.about_name}>Alice Waters</h3>
+            <p className={styles.about_text}>
+              Alice Waters is a celebrated American chef, restaurateur, author,
+              and activist known for her dedication to sustainable, locally
+              sourced food. She founded Chez Panisse, a famed Berkeley
+              restaurant that embodies the farm-to-table philosophy. Waters
+              emphasizes fresh, organic ingredients from local producers who
+              prioritize environmental care, transforming how people think about
+              food and its connection to the environment.
+            </p>
 
-              <button className={styles.about_button}>View More</button>
-            </div>
-
-            <div className={styles.test}>
-              <ul className={styles.block_wrapper}>
-                <li className={styles.block}>
-                  <p className={styles.block_title}>10+</p>
-                  <p className={styles.block_text}>Experience</p>
-                </li>
-
-                <li className={styles.block}>
-                  <p className={styles.block_title}>20+</p>
-                  <p className={styles.block_text}>Awards & Certificate</p>
-                </li>
-              </ul>
-            </div>
+            <button className={styles.about_button}>View More</button>
           </div>
         </div>
+
+        <ul className={styles.block_wrapper}>
+          <li className={styles.block}>
+            <p className={styles.block_title}>10+</p>
+            <p className={styles.block_text}>Experience</p>
+          </li>
+
+          <li className={styles.block}>
+            <p className={styles.block_title}>20+</p>
+            <p className={styles.block_text}>Awards & Certificate</p>
+          </li>
+        </ul>
       </div>
     </section>
   );
