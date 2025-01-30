b65503cd35ee414175704dc332b8a227710b1c43
Yevhenii Pozniak <jekamanu@gmail.com>
2025-01-30 15:48:40 +0200

    update fixed in About section

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from './About.module.css';
-import chef from '../../../../public/image/About/About.jpg';
+import chef from '../../../../public/image/About/about.jpg';
 
 export default function About() {
   return (
@@ -8,41 +8,38 @@ export default function About() {
       <h2 className={styles.title}>Meet the Chief</h2>
       <div className={styles.wrapper}>
         <div className={styles.first_wrapper}>
-          <div className={styles.test777}>
-            <div className={styles.image_wrapper}>
-              <Image src={chef} alt="chef's photo" className={styles.image} />
+          <div className={styles.image_wrapper}>
+            <Image src={chef} alt="chef's photo" className={styles.image} />
+          </div>
+
+          <div className={styles.second_wrapper}>
+            <div className={styles.about_wrapper}>
+              <h3 className={styles.about_name}>Alice Waters</h3>
+              <p className={styles.about_text}>
+                Alice Waters is a celebrated American chef, restaurateur,
+                author, and activist known for her dedication to sustainable,
+                locally sourced food. She founded Chez Panisse, a famed Berkeley
+                restaurant that embodies the farm-to-table philosophy. Waters
+                emphasizes fresh, organic ingredients from local producers who
+                prioritize environmental care, transforming how people think
+                about food and its connection to the environment.
+              </p>
+
+              <button className={styles.about_button}>View More</button>
             </div>
 
-            <div className={styles.second_wrapper}>
-              <div className={styles.about_wrapper}>
-                <h3 className={styles.about_name}>Alice Waters</h3>
-                <p className={styles.about_text}>
-                  Alice Waters is a celebrated American chef, restaurateur,
-                  author, and activist known for her dedication to sustainable,
-                  locally sourced food. She founded Chez Panisse, a famed
-                  Berkeley restaurant that embodies the farm-to-table
-                  philosophy. Waters emphasizes fresh, organic ingredients from
-                  local producers who prioritize environmental care,
-                  transforming how people think about food and its connection
-                  to the environment.
-                </p>
+            <div className={styles.test}>
+              <ul className={styles.block_wrapper}>
+                <li className={styles.block}>
+                  <p className={styles.block_title}>10+</p>
+                  <p className={styles.block_text}>Experience</p>
+                </li>
 
-                <button className={styles.about_button}>View More</button>
-              </div>
-              
-              <div className={styles.test}>
-                <ul className={styles.block_wrapper}>
-                  <li className={styles.block}>
-                    <p className={styles.block_title}>10+</p>
-                    <p className={styles.block_text}>Experience</p>
-                  </li>
-
-                  <li className={styles.block}>
-                    <p className={styles.block_title}>20+</p>
-                    <p className={styles.block_text}>Awards & Certificate</p>
-                  </li>
-                </ul>
-              </div>
+                <li className={styles.block}>
+                  <p className={styles.block_title}>20+</p>
+                  <p className={styles.block_text}>Awards & Certificate</p>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
